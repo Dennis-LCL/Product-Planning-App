08c66cf197403fa3cf8e3404f69d821d464f4f6b
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-06-13 23:23:12 +0800

    Put PromoCalendar and ForecastAssumptions components under PromoPlanner component and integrate PromoPlanner into App.js.

---
+++ src/components/PromoPlanner.js
@@ -0,0 +1,135 @@
+import React from "react";
+import PromoCalendar from "../components/PromoCalendar";
+import ForecastAssumptions from "../components/ForecastAssumptions";
+
+class PromoPlanner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Promo Planner</h1>
+        <PromoCalendar
+          productMaster={mockProductMaster}
+          promoTypes={mockPromoTypes}
+        />
+        <ForecastAssumptions productPromoTypeId="" algorithm={mockAlgorithm} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default PromoPlanner;
+
+const mockProductMaster = [
+  {
+    Brand: "Boldie",
+    Group: "Shampoo 250ml",
+    Code: "A01",
+    Description: "Drop Your Hair Shampoo"
+  },
+  {
+    Brand: "Boldie",
+    Group: "Shampoo 250ml",
+    Code: "A02",
+    Description: "Scratch Your Sculp Shampoo"
+  },
+  {
+    Brand: "Boldie",
+    Group: "Shampoo 250ml",
+    Code: "A03",
+    Description: "Burn The Root Shampoo"
+  },
+  {
+    Brand: "Boldie",
+    Group: "Shampoo 250ml",
+    Code: "A04",
+    Description: "Let It Snow Shampoo"
+  },
+  {
+    Brand: "Boldie",
+    Group: "Shampoo 250ml",
+    Code: "A05",
+    Description: "Volumn Reduction Shampoo"
+  }
+];
+
+const mockPromoTypes = ["10% Off", "30% Off", "50% Off"];
+
+const mockAlgorithm = [
+  {
+    ID: "A01-10%",
+    Product: "Drop Your Hair Shampoo",
+    PromoType: "10% Off",
+    KPIs: {
+      ScanUnit: 120,
+      BaselineUnit: 100,
+      IncrementalUnit: 20,
+      IncrementalGIV: 300,
+      BaseListPrice: 15,
+      TotalCost: 360,
+      ScanDealUnitCost: 3,
+      TotalBudget: 450,
+      FundRate: 0.25,
+      NetSufficiency: 90,
+      ROI: 1.83
+    }
+  },
+  {
+    ID: "A01-30%",
+    Product: "Drop Your Hair Shampoo",
+    PromoType: "30% Off",
+    KPIs: {
+      ScanUnit: 300,
+      BaselineUnit: 100,
+      IncrementalUnit: 200,
+      IncrementalGIV: 3000,
+      BaseListPrice: 15,
+      TotalCost: 2700,
+      ScanDealUnitCost: 9,
+      TotalBudget: 1125,
+      FundRate: 0.25,
+      NetSufficiency: -1575,
+      ROI: 1.11
+    }
+  },
+  {
+    ID: "A01-50%",
+    Product: "Drop Your Hair Shampoo",
+    PromoType: "50% Off",
+    KPIs: {
+      ScanUnit: 1000,
+      BaselineUnit: 100,
+      IncrementalUnit: 900,
+      IncrementalGIV: 13500,
+      BaseListPrice: 15,
+      TotalCost: 15000,
+      ScanDealUnitCost: 15,
+      TotalBudget: 3750,
+      FundRate: 0.25,
+      NetSufficiency: -11250,
+      ROI: 0.9
+    }
+  },
+  {
+    ID: "A01-NPW",
+    Product: "Drop Your Hair Shampoo",
+    PromoType: "Non-Promo Week",
+    KPIs: {
+      ScanUnit: 100,
+      BaselineUnit: 100,
+      IncrementalUnit: 0,
+      IncrementalGIV: 0,
+      BaseListPrice: 15,
+      TotalCost: 0,
+      ScanDealUnitCost: 0,
+      TotalBudget: 375,
+      FundRate: 0.25,
+      NetSufficiency: 375,
+      ROI: 0
+    }
+  }
+];
