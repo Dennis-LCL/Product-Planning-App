e66301b74a86a7dbc4bbc030f3eb212dc46ee5ae
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-06-17 11:58:41 +0800

    Reduce number of mock products in App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -202,4 +202,76 @@ const mockAlgorithm = [
       ROI: 0.9
     }
   }
+  // {
+  //   ID: "A03-NPW",
+  //   Product: "Burn The Root Shampoo",
+  //   PromoType: "Non-Promo Week",
+  //   KPIs: {
+  //     ScanUnit: 100,
+  //     BaselineUnit: 100,
+  //     IncrementalUnit: 0,
+  //     IncrementalGIV: 0,
+  //     BaseListPrice: 15,
+  //     TotalCost: 0,
+  //     ScanDealUnitCost: 0,
+  //     TotalBudget: 375,
+  //     FundRate: 0.25,
+  //     NetSufficiency: 375,
+  //     ROI: 0
+  //   }
+  // },
+  // {
+  //   ID: "A03-10% Off",
+  //   Product: "Burn The Root Shampoo",
+  //   PromoType: "10% Off",
+  //   KPIs: {
+  //     ScanUnit: 120,
+  //     BaselineUnit: 100,
+  //     IncrementalUnit: 20,
+  //     IncrementalGIV: 300,
+  //     BaseListPrice: 15,
+  //     TotalCost: 360,
+  //     ScanDealUnitCost: 3,
+  //     TotalBudget: 450,
+  //     FundRate: 0.25,
+  //     NetSufficiency: 90,
+  //     ROI: 1.83
+  //   }
+  // },
+  // {
+  //   ID: "A03-30% Off",
+  //   Product: "Burn The Root Shampoo",
+  //   PromoType: "30% Off",
+  //   KPIs: {
+  //     ScanUnit: 300,
+  //     BaselineUnit: 100,
+  //     IncrementalUnit: 200,
+  //     IncrementalGIV: 3000,
+  //     BaseListPrice: 15,
+  //     TotalCost: 2700,
+  //     ScanDealUnitCost: 9,
+  //     TotalBudget: 1125,
+  //     FundRate: 0.25,
+  //     NetSufficiency: -1575,
+  //     ROI: 1.11
+  //   }
+  // },
+  // {
+  //   ID: "A03-50% Off",
+  //   Product: "Burn The Root Shampoo",
+  //   PromoType: "50% Off",
+  //   KPIs: {
+  //     ScanUnit: 1000,
+  //     BaselineUnit: 100,
+  //     IncrementalUnit: 900,
+  //     IncrementalGIV: 13500,
+  //     BaseListPrice: 15,
+  //     TotalCost: 15000,
+  //     ScanDealUnitCost: 15,
+  //     TotalBudget: 3750,
+  //     FundRate: 0.25,
+  //     NetSufficiency: -11250,
+  //     ROI: 0.9
+  //   }
+  // }
 ];
